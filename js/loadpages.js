798d3d30b96e186477ff25ecb2bea3daeaef3621
lucasliet <lucasouliveira@gmail.com>
2019-11-15 00:39:32 -0300

    sessionStorage pra salvar página atual

--- js/loadpages.js
+++ js/loadpages.js
@@ -1,4 +1,12 @@
+if (sessionStorage.getItem('pagina')) {
+    loadPage(sessionStorage.getItem('pagina'));
+
+} else {
+    loadPage('pages/home.html')
+}
+
 function loadPage(page) {
+    sessionStorage.setItem('pagina', page);
     var xhr = new XMLHttpRequest();
     xhr.open('GET', page, true);
     xhr.onreadystatechange = function () {
