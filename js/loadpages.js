6315d47b917c4c15c07a38b1cca096c3989cdd17
Leonardo Gil <leogilc@gmail.com>
2019-11-16 14:50:42 -0300

    correcao banner cardapio e footer

--- js/loadpages.js
+++ js/loadpages.js
@@ -11,16 +11,6 @@ function loadPage(page) {
     xhr.open('GET', page, true);
     xhr.onreadystatechange = function () {
         document.getElementById('corpo').innerHTML = this.responseText;
-        // if (page === 'pages/produtos.html') {
-        //     var galeria = "";
-
-        //     for (let i = 1; i <= 6; i++) {
-        //         galeria += `
-                    
-        //             `
-        //     }
-        //     document.getElementById('burgerStore').innerHTML = galeria;
-        // }
         document.documentElement.scrollTop = 0;
     };
     xhr.send();
